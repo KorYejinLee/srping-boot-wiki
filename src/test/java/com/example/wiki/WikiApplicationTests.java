9f112929f1bcbf75a36a05b198e9ee6b3b015104
Koryjlee <koryjlee@gmail.com>
2024-10-21 14:41:29 +0900

    repository 생성 후 test 환경에서 question 질문 2개 저장

--- src/test/java/com/example/wiki/WikiApplicationTests.java
+++ src/test/java/com/example/wiki/WikiApplicationTests.java
@@ -3,11 +3,30 @@ package com.example.wiki;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-class WikiApplicationTests {
+import java.time.LocalDateTime;
 
-	@Test
-	void contextLoads() {
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+class SbbApplicationTests {
+
+	@Autowired // 스프링의 '의존성 주입(DI)' 기능
+	private QuestionRepository questionRepository;
+
+	@Test // testJpa 메서드가 테스트 매서드임을 나타냄
+	void testJpa() {
+		Question q1 = Question.builder()
+				.subject("sbb가 무엇인가요?")
+				.content("sbb에 대해서 알고 싶습니다.")
+				.createDate(LocalDateTime.now())
+				.build();
+		this.questionRepository.save(q1); // 첫번째 질문 저장
+
+		Question q2 = Question.builder()
+				.subject("스프링부트 모델 질문입니다.")
+				.content("id는 자동으로 생성되나요?")
+				.createDate(LocalDateTime.now())
+				.build();
+		this.questionRepository.save(q2); // 두번째 질문 저장
 	}
-
 }
