5dc7b244d3527bd600e0265d7956b311dec0fd3d
Koryjlee <koryjlee@gmail.com>
2024-10-21 15:18:03 +0900

    findById()

--- src/test/java/com/example/wiki/WikiApplicationTests.java
+++ src/test/java/com/example/wiki/WikiApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.wiki;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +16,12 @@ class SbbApplicationTests {
 
 	@Test // testJpa 메서드가 테스트 매서드임을 나타냄
 	void testJpa() {
-		List<Question> all = this.questionRepository.findAll(); // question 테이블에 저장된 모든 데이터를 조회
-		// SELECT * FROM QUESTION
-		assertEquals(2, all.size()); // assertEquals(기댓값, 실젯값)
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		// Return type Question이 아닌 Optional 이다
+		// Optional은 그 값을 처리하기 위한(null값을 유연하게 처리하기 위한) 클래스
+		if(oq.isPresent()) { // 값이 존재하는지 확인
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 }
