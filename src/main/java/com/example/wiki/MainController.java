3703761fcb9579d4365a136eb85e4e010f0ce6b6
Koryjlee <koryjlee@gmail.com>
2024-10-21 12:20:58 +0900

    500 ERROR 해결

--- src/main/java/com/example/wiki/MainController.java
+++ src/main/java/com/example/wiki/MainController.java
@@ -2,11 +2,14 @@ package com.example.wiki;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+
     @GetMapping("/sbb")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody // URL 요청에 대한 응답으로 문자열을 리턴하라
+    public String index() {
+        return "안녕하세요 sbb에 오신 것을 환영합니다.";
     }
 }
