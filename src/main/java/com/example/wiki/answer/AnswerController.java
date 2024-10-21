56824c2601b4fcfda1e35a55e523dbb1ea356932
Koryjlee <koryjlee@gmail.com>
2024-10-21 19:36:47 +0900

    답변 서비스 만들기

--- src/main/java/com/example/wiki/answer/AnswerController.java
+++ src/main/java/com/example/wiki/answer/AnswerController.java
@@ -16,12 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AnswerController {
 
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     // POST 요청을 처리하는 경우에 사용
     @PostMapping("/create/{id}") // POST 방식은 주로 데이터를 저장하는 용도로 사용한다는 점 상기
     public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
         Question question = this.questionService.getQuestion(id);
-        // TODO: 답변을 저장한다.
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
---
+++ src/main/java/com/example/wiki/answer/AnswerService.java
@@ -0,0 +1,26 @@
+package com.example.wiki.answer;
+
+import com.example.wiki.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    // 입력받은 2개의 매개변수인 question과 content를 사용하여 Answer 객체를 생성하여 저장
+    public void create(Question question, String content) {
+        Answer answer = Answer.builder()
+                .content(content)
+                .createDate(LocalDateTime.now())
+                .question(question)
+                .build();
+
+        this.answerRepository.save(answer);
+    }
+
+}
