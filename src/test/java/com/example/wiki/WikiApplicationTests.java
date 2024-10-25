83a7dd9a3144a3564f07a44cb1bff2f05fd7b9d0
Koryjlee <koryjlee@gmail.com>
2024-10-25 13:06:16 +0900

    docker-compose 사용하여 CI/CD

--- src/test/java/com/example/wiki/WikiApplicationTests.java
+++ src/test/java/com/example/wiki/WikiApplicationTests.java
@@ -1,26 +1,10 @@
 package com.example.wiki;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
-import com.example.wiki.answer.Answer;
-import com.example.wiki.question.Question;
-import com.example.wiki.question.QuestionRepository;
-import com.example.wiki.question.QuestionService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+//@SpringBootTest
 class WikiApplicationTests {
-
-	@Autowired
-	private QuestionService questionService;
-
 	@Test
 	void testJpa() {
 	}
